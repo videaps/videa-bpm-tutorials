b41f20e7564ed7a1c0919cef235d397899946d49
oliver <oliver.hock@videa-services.com>
2017-05-22 22:45:38 +0200

    add start event message example

---
+++ src/test/java/services/videa/tutorials/bpm/events/StartEventMessageTest.java
@@ -0,0 +1,60 @@
+/*
+ Copyright (c) 2017 Videa Project Services GmbH
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software,and to permit persons to whom the Software is furnished to do so, 
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial 
+ portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+ NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package services.videa.tutorials.bpm.events;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+@Deployment(resources = { 
+		"events/start-event-message.bpmn"
+		})
+public class StartEventMessageTest {
+
+	@Rule
+	public ProcessEngineRule processEngine = new ProcessEngineRule();
+
+	private RuntimeService runtimeService = null;
+
+	@Before
+	public void setUp() throws Exception {
+		runtimeService = processEngine.getRuntimeService();
+	}
+
+	@Test
+	public void startProcessByMessage() {
+	    ProcessInstance processInstance = runtimeService.startProcessInstanceByMessage("Message_StartEventRecipient");
+		assertThat(processInstance).isEnded();
+	}
+	
+	@Test
+	public void startMainProcessAndCallMessage() {
+		// Participants are started by using method "startProcessInstanceByKey".
+	    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Process_Sender");
+		assertThat(processInstance).isEnded();
+		
+	}
+
+}
